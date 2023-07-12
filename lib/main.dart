ba6f326e92ccfe584253244dac3db8a01f4b1edf
iliyas <iliyasifa@gmail.com>
2023-07-12 13:31:30 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'dart:async';
+
+class MyClass {
+  final List<int> _list = [];
+
+  void addToList(int value) {
+    _list.add(value);
+  }
+
+  void dispose() {
+    _list.clear();
+  }
+}
+
+void main() {
+  final completer = Completer<void>();
+  final myClass = MyClass();
+
+  for (int i = 0; i < 100000; i++) {
+    myClass.addToList(i);
+  }
+
+  // We no longer need the MyClass instance, but it is not garbage collected
+  // because the _list instance variable is still referenced by the MyClass instance.
+
+  myClass.dispose(); // Clear the list to free up memory
+  print(myClass._list);
+
+  // Release the reference to myClass to allow it to be garbage collected
+  Future(
+    () => completer.complete(),
+  ).then(
+    (_) => myClass.dispose(),
+  );
+
+  completer.future.then((_) {
+    // The MyClass instance and its _list instance variable are now eligible
+    // for garbage collection.
+    print('Memory leak avoided');
+  });
+}
